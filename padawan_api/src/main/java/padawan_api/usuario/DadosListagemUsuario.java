aa27884eb74747af7111b391012f5c9aca57e04a
daniloGabriel <danilonascimento.dr@gmail.com>
2023-10-13 18:49:39 -0300

    limpandoRepository

--- padawan_api/src/main/java/padawan_api/usuario/DadosListagemUsuario.java
+++
@@ -1,8 +0,0 @@
-package padawan_api.usuario;
-
-public record DadosListagemUsuario(Long id, String username) {
-
-    public DadosListagemUsuario(Usuario usuario){
-        this(usuario.getId(), usuario.getUsername());
-    }
-}
--- padawan_api/src/main/java/padawan_api/usuario/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package padawan_api.usuario;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
