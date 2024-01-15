83153b9045a3b6924666c471bc8c41e113b29bf0
daniloGabriel <danilonascimento.dr@gmail.com>
2024-01-14 21:04:56 -0300

    I:EmailService

--- padawan_api/src/main/java/padawan_api/service/email/EmailService.java
+++ padawan_api/src/main/java/padawan_api/service/email/EmailService.java
@@ -1,5 +1,28 @@
 package padawan_api.service.email;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import padawan_api.model.email.DadosEmailDTO;
+
+@Service
 public class EmailService {
+
+    private JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender){
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(DadosEmailDTO email){
+        
+        var message = new SimpleMailMessage();
+        message.setFrom("danilonascimento.dr@gmail.com");
+        message.setTo(email.to());
+        message.setSubject(email.subjec());
+        message.setText(email.body());
+        mailSender.send(message);
+    }
     
 }
